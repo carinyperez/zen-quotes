1b5a98d776f5d2fcd23848514dd5af96e7784d8d
Cariny Perez <perezcariny@gmail.com>
2021-08-09 17:19:44 -0700

    Get quote data using fetch api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Player from './components/player.jsx';
 
-
-
 function App() {
   return (
     <div className="App">
--- src/components/player.jsx
+++ src/components/player.jsx
@@ -2,16 +2,38 @@ import './player.styles.scss';
 import calm from '../assets/calm.mp4'; 
 import ReactPlayer from 'react-player';
 
+
+// fetch data 
+// filter data using regex 
+
+
+const fetchQuote = () =>  {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = 'https://zenquotes.io/api/quotes'
+    fetch(`${proxyurl}${url}`)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+}
+
+fetchQuote();
+
 const Player = () => {
+
     return (
-        <div>
+        <div className='cover'>
         <ReactPlayer 
         className='player' 
+        width='null'
+        height='null'
         url={calm} 
         playing 
         loop 
         muted
         ></ReactPlayer>
+        <input
+        type='text' value='Search Bar'
+       ></input>
         </div>
     )
 }
